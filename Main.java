493d7ce845a683e5f00b9409262dfc842d15b967
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2024-04-21 12:40:43 +0530

    added bubble sort and selection sort

---
+++ Main.java
@@ -0,0 +1,8 @@
+import array.bubble_sort.BubbleSort;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.bubbleSort();
+    }
+}
---
+++ array/README.md
@@ -0,0 +1,2 @@
+# Arrays
+An array is a data structure used to store multiple elements.
